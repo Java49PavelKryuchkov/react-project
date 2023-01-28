af49cfc6167ed22ce2e3ef0a0d7ae967ff927401
Pavel16rus <kryuchkov.p@gmail.com>
2023-01-28 21:12:26 +0200

    LALALA

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,30 @@ import { Timer } from './components/Timer';
 
 
 function App() {
-  
+    const [citiesList, setCitiesList] = React.useState<string[]>([]); //why there'not useEffect hook?
+    function inputProcessFun(value:string): string {
+        const cities: string[] = value.split("#");
+        const res: string = '';
+        setCitiesList(cities.slice());
+        return res;
+    }
+    function createDivs(): JSX.Element[] {
+    const res: JSX.Element[] = []; //why JSX.element and not just []
+    for(let i = 0; i < citiesList.length; i+=2) {
+        res.push(<div>
+          <Timer cityOrCountry={citiesList[i]} />
+          <Timer cityOrCountry={citiesList[i + 1]} /> 
+        </div>)
+    }
+    return res;
+    }
+    return <div style={flexColumn}>
+    <Input placeHolder={'enter city/countries separated by #'}
+     processInput={inputProcessFun}/> //   WHAT EXACTLY DO WE PASS THROUGH inputProcessFun()
+     {createDivs()}
+    
+
+  </div>
 }
 
 export default App;
