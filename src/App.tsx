50b31306a5b219c868c2be42d4f6501042333792
Pavel16rus <kryuchkov.p@gmail.com>
2023-02-01 20:43:40 +0200

    initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Life } from './components/Life';
-import lifeConfig from './config/life-game.json'
 
 function App() {
     return <div>
-      <Life dimensions={lifeConfig.dimensions} tickInterval={lifeConfig.tickInterval}></Life>
   </div>
 }
 
